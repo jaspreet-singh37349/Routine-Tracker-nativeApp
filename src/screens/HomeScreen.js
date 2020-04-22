ca51de3f01ae44636e3708e525c342fb5378f1e8
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-04-22 21:00:41 +0530

    basic initial project added

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import styles from '../style';
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>HomeScreen</Text>;
+};
+
+
+
+export default HomeScreen;
---
+++ src/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    text: {
+      fontSize: 50,
+      backgroundColor: '#009',
+      marginTop: 50,
+      color: "green"
+    }
+  });
