5308846e346b1e4c3c8fb6747ff80426426a6e3d
jaspreet-singh37349 <jaspreet.singh37349@gmail.com>
2020-05-26 20:20:35 +0530

    custom statusbar added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -7,8 +7,17 @@ import reducer from './reducers'
 import History from './components/History'
 import { createMaterialTopTabNavigator } from 'react-navigation-tabs'
 import {createAppContainer} from 'react-navigation'
-import { purple, white } from './utils/colors'
+import { purple, white, red } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import Constants from 'expo-constants'
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tabs = createMaterialTopTabNavigator({
   History: {
@@ -51,7 +60,10 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <AppContainer />
+        <View style={{flex:1}}>
+          <UdaciStatusBar backgroundColor={red} barStyle="light-content" />
+          <AppContainer />
+        </View>
       </Provider>
     )
   }
